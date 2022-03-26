3658c2c7f7763d6d25390fccd1d3e8c50b494f8b
Ayman Mukhaimar <38997266+AymanMukh@users.noreply.github.com>
2022-03-26 18:39:33 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,115 +5,6 @@
 
 // All code: https://editor.p5js.org/codingtrain/sketches/JoZl-QRPK
 
-// Separated into three sketches
-// 1: Data Collection: https://editor.p5js.org/codingtrain/sketches/kTM0Gm-1q
-// 2: Model Training: https://editor.p5js.org/codingtrain/sketches/-Ywq20rM9
-// 3: Model Deployment: https://editor.p5js.org/codingtrain/sketches/c5sDNr8eM
-
-// let video;
-// let poseNet;
-// let pose;
-// let skeleton;
-//
-// let brain;
-// let poseLabel = "Y";
-
-// function setup() {
-//   createCanvas(640, 480);
-//   video = createCapture(VIDEO);
-//   video.hide();
-//   poseNet = ml5.poseNet(video, modelLoaded);
-//   poseNet.on('pose', gotPoses);
-//
-//   let options = {
-//     inputs: 34,
-//     outputs: 4,
-//     task: 'classification',
-//     debug: true
-//   }
-//   brain = ml5.neuralNetwork(options);
-//   const modelInfo = {
-//     model: 'model2/model.json',
-//     metadata: 'model2/model_meta.json',
-//     weights: 'model2/model.weights.bin',
-//   };
-//   brain.load(modelInfo, brainLoaded);
-// }
-//
-// function brainLoaded() {
-//   console.log('pose classification ready!');
-//   classifyPose();
-// }
-//
-// function classifyPose() {
-//   if (pose) {
-//     let inputs = [];
-//     for (let i = 0; i < pose.keypoints.length; i++) {
-//       let x = pose.keypoints[i].position.x;
-//       let y = pose.keypoints[i].position.y;
-//       inputs.push(x);
-//       inputs.push(y);
-//     }
-//     brain.classify(inputs, gotResult);
-//   } else {
-//     setTimeout(classifyPose, 100);
-//   }
-// }
-//
-// function gotResult(error, results) {
-//
-//   if (results[0].confidence > 0.75) {
-//     poseLabel = results[0].label.toUpperCase();
-//   }
-//   //console.log(results[0].confidence);
-//   classifyPose();
-// }
-//
-//
-// function gotPoses(poses) {
-//   if (poses.length > 0) {
-//     pose = poses[0].pose;
-//     skeleton = poses[0].skeleton;
-//   }
-// }
-//
-//
-// function modelLoaded() {
-//   console.log('poseNet ready');
-// }
-//
-// function draw() {
-//   push();
-//   translate(video.width, 0);
-//   scale(-1, 1);
-//   image(video, 0, 0, video.width, video.height);
-//
-//   if (pose) {
-//     for (let i = 0; i < skeleton.length; i++) {
-//       let a = skeleton[i][0];
-//       let b = skeleton[i][1];
-//       strokeWeight(2);
-//       stroke(0);
-//
-//       line(a.position.x, a.position.y, b.position.x, b.position.y);
-//     }
-//     for (let i = 0; i < pose.keypoints.length; i++) {
-//       let x = pose.keypoints[i].position.x;
-//       let y = pose.keypoints[i].position.y;
-//       fill(0);
-//       stroke(255);
-//       ellipse(x, y, 16, 16);
-//     }
-//   }
-//   pop();
-//
-//   fill(255, 0, 255);
-//   noStroke();
-//   textSize(512);
-//   textAlign(CENTER, CENTER);
-//   text(poseLabel, width / 2, height / 2);
-// }
-//
 
 
 
@@ -204,8 +95,8 @@ function onResults(results) {
 
   let inputs = [];
   for (let i = 0; i < results.poseLandmarks.length; i++) {
-    let x = results.poseLandmarks[i].x-results.poseLandmarks[30].x;
-    let y = results.poseLandmarks[i].y-results.poseLandmarks[30].y;
+    let x = results.poseLandmarks[i].x-results.poseLandmarks[60].x;
+    let y = results.poseLandmarks[i].y-results.poseLandmarks[60].y;
     inputs.push(x);
     inputs.push(y);
   }
